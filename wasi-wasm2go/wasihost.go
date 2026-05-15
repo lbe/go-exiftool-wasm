01633434edbe65b5d4fcb70027a5ac58121dd96c
lbe <1119377+lbe@users.noreply.github.com>
2026-05-14 22:40:30 -0500

    refactor(wasihost-init): normalize formatting and clarify option parameter names
    Review Performed: Results Approved

--- wasi-wasm2go/wasihost.go
+++ wasi-wasm2go/wasihost.go
@@ -25,15 +25,15 @@ func New(mem func() []byte, opts ...Option) *State {
 	return s
 }
 
-func WithArgs(args ...string) Option          { return func(*State) {} }
-func WithEnv(env ...string) Option            { return func(*State) {} }
-func WithMount(path string, root fs.FS) Option { return func(*State) {} }
-func WithWritableMount(path, hostRoot string, root fs.FS) Option { return func(*State) {} }
-func WithStdin(r io.Reader) Option            { return func(*State) {} }
-func WithStdout(w io.Writer) Option           { return func(*State) {} }
-func WithStderr(w io.Writer) Option           { return func(*State) {} }
-func WithTracing() Option                     { return func(*State) {} }
-func WithOwnerAssertion() Option              { return func(*State) {} }
+func WithArgs(args ...string) Option                             { return func(*State) {} }
+func WithEnv(env ...string) Option                               { return func(*State) {} }
+func WithMount(guestPath string, root fs.FS) Option                   { return func(*State) {} }
+func WithWritableMount(guestPath, hostRoot string, root fs.FS) Option { return func(*State) {} }
+func WithStdin(r io.Reader) Option                               { return func(*State) {} }
+func WithStdout(w io.Writer) Option                              { return func(*State) {} }
+func WithStderr(w io.Writer) Option                              { return func(*State) {} }
+func WithTracing() Option                                        { return func(*State) {} }
+func WithOwnerAssertion() Option                                 { return func(*State) {} }
 
 func mapOSError(err error) uint32 {
 	if errors.Is(err, os.ErrNotExist) {
